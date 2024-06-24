4db001b307d179ab24c20fb8224f56a4b446869d
NoahRose123 <154570263+NoahRose123@users.noreply.github.com>
2024-06-23 19:40:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-const Textbox = document.getElementById("Textbox");
+let meats = ["apple","banana","orange","coconut"];
 
-const toFahrenheit = document.getElementById("toFahrenheit");
+meats.sort().reverse();
 
-const toCelsius = document.getElementById("toCelsius");    
+for(let meat of meats){
 
-const result = document.getElementById("result");
-
-let temp;
-     
-function convert(){
-   
-if(toFahrenheit.checked){
-
-temp = Number(Textbox.value);
-
-temp = temp * 9 / 5 +32;
-
-result.textContent = temp.toFixed(1) + "°F";
-}
-
-else if(toCelsius.checked){
-    temp = Number(Textbox.value);
-
-    temp = (temp - 32) * (5/9);
-    
-    result.textContent = temp.toFixed(1) + "°C";
-}
-
-else{
-    result.textContent = "Please Select A Unit";
-}
+console.log(meat);
 
 }
