bec5a9d1451d7c185a0c5d3dfaafefbfc7267a67
NoahRose123 <154570263+NoahRose123@users.noreply.github.com>
2024-07-08 14:01:23 -0700

    Did The Dice Program (Then Deleted It)

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
 
-
-function combineStrings(...strings){
-
-return strings.join(" ");
-
+    for(let i =0; i < numOfDice; i++){
+        const value = Math.floor(Math.random()* 6) + 1; 
+        values.push(value);
+        images.push(`<img src="Dice Images/${value}.png alt=Dice ${value}">`);
+        }                          
+        
+        diceResult.textContent = `dice: ${values.join(', ')}`;
+        diceImages.innerHTML = images.join(` `);
 }
 
-const fullname = combineStrings("Mr","SpongeBob","SquarePants", "III");
-
-console.log(fullname);
